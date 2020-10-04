538edfb888d9b04c2945456ec7e6cda9b6956279
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-10-04 22:41:30 +0200

    finished token list

--- src/ch/fhnw/cpib/compiler/tokens/TokenList.java
+++ src/ch/fhnw/cpib/compiler/tokens/TokenList.java
@@ -1,18 +1,42 @@
 package ch.fhnw.cpib.compiler.tokens;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TokenList implements ITokenList {
+
+    private final List<IToken> list = new LinkedList<IToken>();
+    private Iterator<IToken> listIterator;
+
     @Override
     public void add(IToken token) {
-
+        if (token == null)
+            return;
+        list.add(token);
     }
 
     @Override
     public void reset() {
-
+        list.clear();
     }
 
     @Override
     public IToken nextToken() {
-        return null;
+        if (listIterator == null) {
+            listIterator = list.iterator();
+        }
+        return listIterator.next();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (IToken token : list){
+            sb.append(token.toString());
+            sb.append(",");
+        }
+        sb.setLength(sb.length()-1);
+        return sb.toString();
     }
 }
