c8dec20456009f64cf93b3466093ee643ed0794c
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-11-02 22:52:24 +0100

    added progParam

--- src/ch/fhnw/cpib/compiler/parser/nts/ProgParam.java
+++ src/ch/fhnw/cpib/compiler/parser/nts/ProgParam.java
@@ -9,9 +9,18 @@ import static ch.fhnw.cpib.compiler.tokens.enums.AttributeTerminals.*;
 
 public class ProgParam implements INtsParser {
     private IToken token;
+    private INtsParser optFlowmode;
+    private INtsParser optChangemode;
+    private INtsParser typedIdent;
 
     public ProgParam() throws GrammarError {
         token = Parser.consume(IDENT, CHANGEMODE, FLOWMODE);
+        optFlowmode = null;
+        optChangemode = null;
+        typedIdent = null;
+    }
 
+    public String toString() {
+        return token.getTerminal().toString() + " " + optFlowmode.toString() + " " + optChangemode.toString() + " " + typedIdent.toString();
     }
 }
