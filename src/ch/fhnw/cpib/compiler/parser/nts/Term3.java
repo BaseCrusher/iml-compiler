929096832865e471be2fa9956cef664f37b1f1f2
Damian Zehnder <zehnder@adobe.com>
2020-11-03 16:42:03 +0100

    adding Term3

--- src/ch/fhnw/cpib/compiler/parser/nts/Term3.java
+++ src/ch/fhnw/cpib/compiler/parser/nts/Term3.java
@@ -1,2 +1,40 @@
-package ch.fhnw.cpib.compiler.parser.nts;public class Term3 {
+package ch.fhnw.cpib.compiler.parser.nts;
+
+import ch.fhnw.cpib.compiler.error.GrammarError;
+import ch.fhnw.cpib.compiler.parser.INtsParser;
+import ch.fhnw.cpib.compiler.parser.Parser;
+import ch.fhnw.cpib.compiler.tokens.IToken;
+
+import static ch.fhnw.cpib.compiler.tokens.enums.AttributeTerminals.IDENT;
+import static ch.fhnw.cpib.compiler.tokens.enums.AttributeTerminals.LITERAL;
+import static ch.fhnw.cpib.compiler.tokens.enums.AttributeTerminals.MONOPR;
+import static ch.fhnw.cpib.compiler.tokens.enums.KeywordTerminals.LPAREN;
+
+public class Term3 implements INtsParser {
+    private final IToken token;
+    private final INtsParser factor;
+    private final INtsParser repMultOprFactor;
+
+    public Term3() throws GrammarError {
+        token = Parser.consume(LPAREN, MONOPR, IDENT, LITERAL);
+        factor = null;
+        repMultOprFactor = null;
+    }
+
+    @Override
+    public String toString() {
+        return factor.toString() + " " + repMultOprFactor.toString();
+    }
+
+    public IToken getToken() {
+        return token;
+    }
+
+    public INtsParser getFactor() {
+        return factor;
+    }
+
+    public INtsParser getRepMultOprFactor() {
+        return repMultOprFactor;
+    }
 }
