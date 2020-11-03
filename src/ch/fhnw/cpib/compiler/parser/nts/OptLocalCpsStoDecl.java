f906dbe8918b05bb5a54f3f37971048713958d7e
BaseCrusher <ruzinski.jehuda@outlook.com>
2020-11-03 21:33:22 +0100

    added optLocalCpsStoDecl

---
+++ src/ch/fhnw/cpib/compiler/parser/nts/OptLocalCpsStoDecl.java
@@ -0,0 +1,45 @@
+package ch.fhnw.cpib.compiler.parser.nts;
+
+import ch.fhnw.cpib.compiler.error.GrammarError;
+import ch.fhnw.cpib.compiler.parser.INtsParser;
+import ch.fhnw.cpib.compiler.parser.Parser;
+import ch.fhnw.cpib.compiler.tokens.IToken;
+
+import static ch.fhnw.cpib.compiler.tokens.enums.AttributeTerminals.*;
+import static ch.fhnw.cpib.compiler.tokens.enums.KeywordTerminals.*;
+
+public class OptLocalCpsStoDecl implements INtsParser {
+    private IToken token;
+    private INtsParser cpsStoDecl;
+    private INtsParser epsilon;
+    private String string;
+
+    public OptLocalCpsStoDecl() throws GrammarError {
+        token = Parser.consume(LOCAL, DO);
+        if(token.hasTerminal(IDENT)) {
+            cpsStoDecl = null;
+            string = token.getTerminal().toString() + " " + cpsStoDecl.toString();
+        }
+        else {
+            epsilon = new Epsilon();
+            string = epsilon.toString();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return string;
+    }
+
+    public IToken getToken() {
+        return token;
+    }
+
+    public INtsParser getCpsStoDecl() {
+        return cpsStoDecl;
+    }
+
+    public INtsParser getEpsilon() {
+        return epsilon;
+    }
+}
